31f671a1ea5a56539631c48650035cae3c0c6c17
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-10 20:00:55 +0530

    Updates

---
+++ precache-manifest.a7a0bbca8647ad6d52d9182af88d2e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9629d3f44195eaa9f27f4b128ece57fc",
+    "url": "/Slider-React-Project/index.html"
+  },
+  {
+    "revision": "5486a536392bdda20c13",
+    "url": "/Slider-React-Project/static/css/main.db6e83bf.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/Slider-React-Project/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Slider-React-Project/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5486a536392bdda20c13",
+    "url": "/Slider-React-Project/static/js/main.c6d78514.chunk.js"
+  },
+  {
+    "revision": "17fc1aef0dc1aa61f23b",
+    "url": "/Slider-React-Project/static/js/runtime-main.34e2cc08.js"
+  }
+]);
